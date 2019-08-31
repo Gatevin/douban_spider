eed45985ed61230c386efd0db58cdfb320b90a47
gatevin <gatevin@outlook.com>
2019-09-01 00:33:24 +0800

    add user history

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 
 func main() {
 	fmt.Println("Hello word！")
-	history_collector.DoubanHandler.MainUrl="https://movie.douban.com/"
-	history_collector.DoubanHandler.Test()
+	history_collector.DoubanUserHistoryHandler.MainUrl="https://movie.douban.com/"
+    history_collector.DoubanUserHistoryHandler.Uid = "summermonica"
+    history_collector.DoubanUserHistoryHandler.Uname= "井颯"
+	history_collector.DoubanUserHistoryHandler.FetchHistoryWithUser()
 }
